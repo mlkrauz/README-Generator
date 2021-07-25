55972d081ae97de0f284a668894a19ee2db69355
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-07-25 11:56:39 -0400

    Added the first prompts. Saving my work, then pivotting to dynamic question/choice generation.

---
+++ index.js
@@ -0,0 +1,33 @@
+//The way we were taught to do it
+//const inquirer = require("inquirer");
+
+//the cool ES6 way vscode recommended it be done
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+inquirer.prompt(
+    [
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'checkbox',
+            name: 'tableOfContents',
+            message: 'What would you like included in your Table of Contents?',
+            choices: [
+                {
+                    //does not work
+                    name: `${value.title}${value.hint}`,
+                    value: {
+                        title: 'Description',
+                        hint: ''
+                    }
+                     
+                }
+            ]
+        }
+
+    ]
+)
