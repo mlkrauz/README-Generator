71b1102acc8c1d6ee9c29f792522735ecbd44cd1
ElusiveEntity <AnElusiveEntity@gmail.com>
2021-08-01 23:16:38 -0400

    Added more prompts to questions.js

--- src/questions.js
+++ src/questions.js
@@ -11,17 +11,30 @@ const questionBank = [
         name: 'tableOfContents',
         message: 'What would you like included in your Table of Contents?',
         choices: [
-            choices.createChoice('Description', '', {checked: true}),
-            choices.createChoice('Table of Contents', '', {checked: true}),
-            choices.createChoice('Media', ' --Images and videos'),
-            choices.createChoice('Installation', '', {checked: true}),
-            choices.createChoice('Usage', '', {checked: true}),
-            choices.createChoice('License', '', {checked: true}),
-            choices.createChoice('Contributing', '', {checked: true}),
-            choices.createChoice('Tests', '', {checked: true}),
-            choices.createChoice('Questions', ' --and contact info', {checked: true})
+            choices.createChoice('Description', 'Description', '', {checked: true}),
+            choices.createChoice('Table of Contents', 'Table of Contents', '', {checked: true}),
+            choices.createChoice('Media', 'Media', ' --Images and videos'),
+            choices.createChoice('Installation', 'Installation', '', {checked: true}),
+            choices.createChoice('Usage', 'Usage', '', {checked: true}),
+            choices.createChoice('License', 'License', '', {checked: true}),
+            choices.createChoice('Contributing', 'Contributing', '', {checked: true}),
+            choices.createChoice('Tests', 'Tests', '', {checked: true}),
+            choices.createChoice('Questions', 'Questions', ' --and contact info', {checked: true})
         ]
     },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license applies to your project?',
+        //very limited list of licenses, but whatever
+        choices: [
+            choices.createChoice('MIT', 'MIT'),
+            choices.createChoice('MPL 2.0', 'MPL 2.0', ' (Mozilla Public License 2.0)'),
+            choices.createChoice('GPL v3', 'GPL v3', ' (GNU GPL v3)'),
+            choices.createChoice('CC0', 'CC0', ' (Creative Commons)')
+        ],
+        when: (answers) => answers.tableOfContents.includes('License')
+    }
 ];
 
 module.exports = {
