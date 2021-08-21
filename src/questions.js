4268295ead84467fa69c3b1b328696e17dbf1091
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-08-20 22:52:37 -0400

    Completed questions.js

--- src/questions.js
+++ src/questions.js
@@ -12,15 +12,40 @@ const questionBank = () => [
         message: 'What would you like included in your Table of Contents?',
         choices: [
             choices.createChoice('Description', 'Description', '', {checked: true}),
-            choices.createChoice('Table of Contents', 'Table of Contents', '', {checked: true}),
+            choices.createChoice('Table of Contents', 'Table of Contents', ''),
             choices.createChoice('Media', 'Media', ' --Images and videos'),
             choices.createChoice('Installation', 'Installation', '', {checked: true}),
             choices.createChoice('Usage', 'Usage', '', {checked: true}),
-            choices.createChoice('License', 'License', '', {checked: true}),
+            choices.createChoice('License', 'License', ''),
             choices.createChoice('Contributing', 'Contributing', '', {checked: true}),
-            choices.createChoice('Tests', 'Tests', '', {checked: true}),
-            choices.createChoice('Questions', 'Questions', ' --and contact info', {checked: true})
-        ]
+            choices.createChoice('Tests', 'Tests', ''),
+            choices.createChoice('Questions', 'Questions', ' --and contact info')
+        ],
+        pageSize: 9
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?',
+        when: (answers) => answers.tableOfContents.includes('Description')
+    },
+    {
+        type: 'input',
+        name: 'media',
+        message: 'What directory contains media you would like to embed?',
+        when: (answers) => answers.tableOfContents.includes('Media')
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Describe how to install your project.',
+        when: (answers) => answers.tableOfContents.includes('Installation')
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Describe how to use your project.',
+        when: (answers) => answers.tableOfContents.includes('Usage')
     },
     {
         type: 'list',
@@ -34,6 +59,30 @@ const questionBank = () => [
             choices.createChoice('CC0', 'CC0', ' (Creative Commons)')
         ],
         when: (answers) => answers.tableOfContents.includes('License')
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Describe how (or if) others may contribute to your project.',
+        when: (answers) => answers.tableOfContents.includes('Contributing')
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Describe how to test your project.',
+        when: (answers) => answers.tableOfContents.includes('Tests')
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
+        when: (answers) => answers.tableOfContents.includes('Questions')
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        when: (answers) => answers.tableOfContents.includes('Questions')
     }
 ];
 
