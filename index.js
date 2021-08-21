e1d44414febc11c0c23cd13184f93c5910b578e4
ElusiveEntity <AnElusiveEntity@gmail.com>
2021-08-21 15:39:45 -0400

    Changed handling of table of contents choices in questions.js. Expanded upon index.js.

--- index.js
+++ index.js
@@ -2,18 +2,69 @@ const inquirer = require("inquirer");
 const fs = require('fs');
 const questions = require('./src/questions');
 
-//Prompt questions and recieve answers
+// Prompt questions and recieve answers
 inquirer.prompt(questions)
 .then(
     (answers) => buildReadme(answers)
 )
+.then(
+    (readmeString) => generateReadme(readmeString)
+)
 
 /**
- * //Takes in an inquirer answers object, and builds a readMe string.
+ * // Takes in an inquirer answers object, and builds a readMe string.
  * @param {Object} answers 
  */
 function buildReadme(answers) {
     console.log(answers)
+
+    // Destructure the answers object
+    const { projectName, tableOfContents, description, media, installation,
+        usage, license, contribute, test, github, email
+    } = answers;
+
+    // an object containing/handling the strings to be appended to our readme.
+    let readmeStrings =  {
+        header: `# ${projectName}`,
+        licenseBadge: '',
+        description: `## Description \n ${description}`,
+        tableOfContents: '',
+        media: '',
+        installation: '',
+        usage: '',
+        license: '',
+        contribute: '',
+        test: '',
+        questions: ''
+    }
+
+    /**
+     * @returns a single line of the table of contents in Markdown notation
+     */
+    const single_TOC_Line = (item) => {
+        if (item.use_In_TOC) {
+            // return table of contents line
+            return  `- [${item.title}](## ${item.title}) \n` 
+        } else {
+            // return empty string
+            return ''
+        }
+    }
+
+    // conditionally update our readmeStrings
+    if (tableOfContents) {
+        let newTableOfContents = `## Table of Contents \n`
+
+        // Add all table of content lines and append to 'newTableOfContents'
+        tableOfContents.map(
+            (item) => newTableOfContents += single_TOC_Line(item)
+        )
+
+        readmeStrings.tableOfContents = newTableOfContents
+    }
+
+
+
 }
 
 /**
@@ -21,5 +72,5 @@ function buildReadme(answers) {
  * @param {String} readMeString 
  */
 function generateReadme(readMeString) {
-    
+    console.log("./README.md created!")
 }
--- src/questions.js
+++ src/questions.js
@@ -1,5 +1,82 @@
 const choices = require("./choices");
 
+//Create choices for table of contents selection. I tried to make this as readable as possible, sorry!
+const tableOfContents_choices = [
+    choices.createChoice(
+        'Description',
+        { title: 'Description', use_In_TOC: false },
+        '',
+        {checked: true}
+    ),
+    choices.createChoice(
+        'Table of Contents',
+        { title: 'Table of Contents', use_In_TOC: false },
+        ''
+    ),
+    choices.createChoice(
+        'Media',
+        { title: 'Media', use_In_TOC: false },
+        ' --Images and videos'
+    ),
+    choices.createChoice(
+        'Installation',
+        { title: 'Installation', use_In_TOC: true },
+        '',
+        {checked: true}
+    ),
+    choices.createChoice(
+        'Usage',
+        { title: 'Usage', use_In_TOC: true },
+        '',
+        {checked: true}
+    ),
+    choices.createChoice(
+        'License',
+        { title: 'License', use_In_TOC: true },
+        ''
+    ),
+    choices.createChoice(
+        'Contributing',
+        { title: 'Contributing', use_In_TOC: true },
+        '',
+        {checked: true}
+    ),
+    choices.createChoice(
+        'Tests',
+        { title: 'Tests', use_In_TOC: true },
+        ''
+        ),
+    choices.createChoice(
+        'Questions',
+        { title: 'Questions and Contact Info', use_In_TOC: true },
+        ' --and contact info'
+    )
+]
+
+/**
+ * Validator function. Checks if 'element.title' is equal to 'title'
+ * @param {object} element 
+ * @param {string} title 
+ */
+const containsString = (element, title) => {
+    try {
+        // Check for undefined args
+        if (!element || !title) {
+            throw new Error("Incorrect number of arguments.")
+        }
+
+        // Check types
+        if (typeof element !== 'object' || typeof title !== 'string') {
+            throw new Error("Incorrect type passed.")
+        }
+
+        return element.title === title
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 const questionBank = () => [
     {
         type: 'input',
@@ -9,43 +86,34 @@ const questionBank = () => [
     {
         type: 'checkbox',
         name: 'tableOfContents',
-        message: 'What would you like included in your Table of Contents?',
-        choices: [
-            choices.createChoice('Description', 'Description', '', {checked: true}),
-            choices.createChoice('Table of Contents', 'Table of Contents', ''),
-            choices.createChoice('Media', 'Media', ' --Images and videos'),
-            choices.createChoice('Installation', 'Installation', '', {checked: true}),
-            choices.createChoice('Usage', 'Usage', '', {checked: true}),
-            choices.createChoice('License', 'License', ''),
-            choices.createChoice('Contributing', 'Contributing', '', {checked: true}),
-            choices.createChoice('Tests', 'Tests', ''),
-            choices.createChoice('Questions', 'Questions', ' --and contact info')
-        ],
+        message: 'What would you like included in your readMe?',
+        choices: tableOfContents_choices,
         pageSize: 9
     },
     {
         type: 'input',
         name: 'description',
         message: 'What is the description of your project?',
-        when: (answers) => answers.tableOfContents.includes('Description')
+        when: (answers) => answers.tableOfContents.some( (element) => containsString(element, 'Description') )
     },
+    
     {
         type: 'input',
         name: 'media',
         message: 'What directory contains media you would like to embed?',
-        when: (answers) => answers.tableOfContents.includes('Media')
+        when: (answers) => answers.tableOfContents.some( (element) => containsString(element, 'Media') )
     },
     {
         type: 'input',
         name: 'installation',
         message: 'Describe how to install your project.',
-        when: (answers) => answers.tableOfContents.includes('Installation')
+        when: (answers) => answers.tableOfContents.some( (element) => containsString(element, 'Installation') )
     },
     {
         type: 'input',
         name: 'usage',
         message: 'Describe how to use your project.',
-        when: (answers) => answers.tableOfContents.includes('Usage')
+        when: (answers) => answers.tableOfContents.some( (element) => containsString(element, 'Usage') )
     },
     {
         type: 'list',
@@ -58,31 +126,31 @@ const questionBank = () => [
             choices.createChoice('GPL v3', 'GPL v3', ' (GNU GPL v3)'),
             choices.createChoice('CC0', 'CC0', ' (Creative Commons)')
         ],
-        when: (answers) => answers.tableOfContents.includes('License')
+        when: (answers) => answers.tableOfContents.some( (element) => containsString(element, 'License') )
     },
     {
         type: 'input',
         name: 'contribute',
         message: 'Describe how (or if) others may contribute to your project.',
-        when: (answers) => answers.tableOfContents.includes('Contributing')
+        when: (answers) => answers.tableOfContents.some( (element) => containsString(element, 'Contributing') )
     },
     {
         type: 'input',
         name: 'test',
         message: 'Describe how to test your project.',
-        when: (answers) => answers.tableOfContents.includes('Tests')
+        when: (answers) => answers.tableOfContents.some( (element) => containsString(element, 'Tests') )
     },
     {
         type: 'input',
         name: 'github',
         message: 'What is your github username?',
-        when: (answers) => answers.tableOfContents.includes('Questions')
+        when: (answers) => answers.tableOfContents.some( (element) => containsString(element, 'Questions and Contact Info') )
     },
     {
         type: 'input',
         name: 'email',
         message: 'What is your email?',
-        when: (answers) => answers.tableOfContents.includes('Questions')
+        when: (answers) => answers.tableOfContents.some( (element) => containsString(element, 'Questions and Contact Info') )
     }
 ];
 
